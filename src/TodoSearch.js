24a0828d187e169a864f4499da81ac2094fd8b0e
josuenc <josue.naure.c@gmail.com>
2022-08-30 12:08:44 -0500

    CLASE 09: MANEJO DEL ESTADO

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
-  return <input className="TodoSearch" placeholder="Cebolla" />;
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+
+  return [
+    <input
+      className="TodoSearch"
+      placeholder="Cebolla"
+      onChange={onSearchValueChange}
+    />,
+    <p>{searchValue}</p>,
+  ];
 }
 
 export { TodoSearch };
